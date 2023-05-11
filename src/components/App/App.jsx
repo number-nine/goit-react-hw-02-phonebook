957f89d19c2fc1342d29711e8683430947b03c23
number-nine <sergii.pokhylko@gmail.com>
2023-05-11 17:30:37 +0300

    without delete

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import { Container } from 'components/App/App.styled';
 
@@ -18,28 +19,33 @@ class App extends Component {
 
   RANDOM_PHONEBOOK_DATA = {
     contacts: [
-    {id: nanoid(), name: 'Rosie Simpson', number: '459-12-56'},
-    {id: nanoid(), name: 'Hermione Kline', number: '443-89-12'},
-    {id: nanoid(), name: 'Eden Clements', number: '645-17-79'},
-    {id: nanoid(), name: 'Annie Copeland', number: '227-91-26'},
-  ]
-  }
+      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
+    ],
+  };
 
   handleFormSubmit = e => {
     e.preventDefault();
-    this.setState(currentState => {
-      return {
-        contacts: [
-          ...currentState.contacts,
-          {
-            id: nanoid(),
-            name: currentState.name,
-            number: currentState.number,
-          },
-        ],
-      };
-    });
-    this.resetForm();
+    if (this.isNameUniq(this.state.name)) {
+      this.setState(currentState => {
+        return {
+          contacts: [
+            ...currentState.contacts,
+            {
+              id: nanoid(),
+              name: currentState.name,
+              number: currentState.number,
+            },
+          ],
+        };
+      });
+      this.resetForm();
+    } else {
+      Notify.failure(`${this.state.name} is already in contacts.`);
+      // console.log();
+    }
   };
 
   handleFillPhonebook = e => {
@@ -52,8 +58,12 @@ class App extends Component {
         ],
       };
     });
+  };
 
-  }
+  isNameUniq = nameToAdd =>
+    !this.state.contacts
+      .map(({ name }) => name.toLowerCase())
+      .includes(nameToAdd.toLowerCase());
 
   resetForm = () => {
     this.setState({ ...this.INITIAL_FORM_STATE });
@@ -63,6 +73,17 @@ class App extends Component {
     const { name, value } = e.target;
     this.setState({ [name]: value });
   };
+
+  handleDeleteContact = (e) => {
+    e.preventDefault();
+    // console.dir(currentState.contacts);
+    this.setState(currentState => 
+    {console.log(currentState.contacts);
+      return currentState.contacts
+        .filter((contact) =>
+          (contact.id !== e.currentTarget.id))}
+    );
+  }
 
   render() {
     return (
@@ -88,7 +109,9 @@ class App extends Component {
           />
           <button type="submit">Add contact</button>
         </form>
-        <button type="submit" onClick={this.handleFillPhonebook}>Fill phonebook</button>
+        <button type="submit" onClick={this.handleFillPhonebook}>
+          Fill phonebook
+        </button>
         <input
           type="text"
           name="filter"
@@ -97,11 +120,27 @@ class App extends Component {
         />
         <h2>Contacts</h2>
         <ul>
-          {this.state.contacts.map(contact => (
-            <li key={contact.id}>
-              {contact.name}: {contact.number}
-            </li>
-          ))}
+          {this.state.contacts
+            .filter(({ name }) =>
+              name
+                .toLowerCase()
+                .includes(this.state.filter.trim().toLowerCase())
+            )
+            .map(contact => {
+              return (
+                <li key={contact.id}>
+                  {contact.name}: {contact.number}
+                  <button
+                    type="button"
+                    id={contact.id}
+                    onClick={this.handleDeleteContact}
+                  >
+                    Delete
+                  </button>
+                </li>
+              );
+              // }
+            })}
         </ul>
       </Container>
     );
